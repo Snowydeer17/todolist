87f2e9f2b036c373224fb8aeccc7ba196c6a42d4
Chelsea J Vick <cjv7mm@hotmail.com>
2026-03-04 11:47:34 -0800

    Add files via upload

--- todolist_chelseavick/app.js
+++ todolist_chelseavick/app.js
@@ -16,8 +16,8 @@ function makeId() {
 }
 
 function save() {
-    const data = { todos, currentFilter };
-    localStorage,setItem(STORAGE_KEY, JSON.stringify(data));
+    const data = { todos, currentfilter };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 
 }
 
@@ -42,11 +42,11 @@ function load() {
     }
 } 
 
-function getVisableTodos () {
-    if ( currentFilter == "active") {
+function getVisableTodos() {
+    if ( currentfilter == "active") {
         return todos.filter(t => !t.completed);
     }
-    if (currentFilter === "completed") {
+    if (currentfilter === "completed") {
         return todos.filter(t => t.completed);
     }
     return todos; 
@@ -69,17 +69,80 @@ function render() {
 
 
     list.innerHTML = visible.map(todos => `
-          <li class="item" data-id"${todos.id}">
+          <li class="item" data-id="${todos.id}">
           <input type="checkbox" data-action="toggle" ${todos.completed ? "checked" : "" } />
-          span class="text" ${todos.completed ? "done" : ""}>
+          <span class="text ${todos.completed ? "done" : ""}">
                ${escapeHtml(todos.text)}
                </span>
             <div class="actions">
-            <button type="button" class="small" data-actions="delete">Delete</button>
+            <button type="button" class="small" data-action="delete">Delete</button>
             </div>
           </li>
         `).join("");
 
 
 save();
+
     }
+
+    function escapeHtml(str) {
+        return String(str)
+        .replaceAll("&","&amp;")
+        .replaceAll("<","&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"',"&quot;")
+        .replaceAll("'","&#039;")
+
+    }
+
+
+    load();
+    render();
+
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const text = input.value.trim();
+        if (!text) {
+            return;
+        }
+
+        todos.unshift({
+            id: makeId(),
+            text,
+            complete: false,
+        });
+
+        input.value = "";
+        input.focus();
+        render();
+
+
+    });
+
+    list.addEventListener("click", function(e) {
+        const li = e.target.closest(".item");
+        if (!li) {
+            return;
+        }
+
+        const id = li.dataset.id;
+        const action = e.target.dataset.action;
+
+        if (action === "toggle") {
+            const todo = todos.find(t => t.id === id);
+            if (!todo) {
+                return;
+
+            }
+
+            todo.completed = !todo.completed;
+            render();
+        }
+
+        if (action === "delete") {
+            todos = todos.filter(t => t.id === id);
+            render();
+        }
+
+    })
